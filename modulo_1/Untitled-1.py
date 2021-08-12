5541d677bbe051acfc578374a443fb93018f3666
joaovictormotaa <joaovictormotaa@arquitetura.ufc.br>
2021-08-12 19:09:39 -0300

    aula_exe mod 1

--- modulo_1/Untitled-1.py
+++ modulo_1/Untitled-1.py
@@ -1,5 +1,16 @@
 import pandas as pd
 import glob
 
-lista_arquivos = glob.glob('D:\arqurb\cursos\python-para-inov-civ\python-inovacao-civica\modulo_1\conjunto_dados/*.csv')
-print(lista_arquivos)
+lista_arquivos = glob.glob('modulo_1\conjunto_dados/*.csv')
+#print(lista_arquivos)
+
+lista_df = []
+
+for csv in lista_arquivos:
+    df = pd.read_csv(csv, sep=';', encoding='latin_1')
+    lista_df.append(df)
+
+#print(lista_df)
+
+candidaturas_PE_RN = pd.concat(lista_df, axis=0, ignore_index=True)
+
